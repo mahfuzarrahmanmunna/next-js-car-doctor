349a002fd59cb4fa53101bdcb7d4c8ad3a5c56e0
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-08-02 21:09:15 +0600

    added services dbConnect and other document

--- src/app/components/ServicesSection/ServicesSection.jsx
+++ src/app/components/ServicesSection/ServicesSection.jsx
@@ -1,30 +1,35 @@
-'use client';
+// app/components/ServicesSection.tsx (or jsx)
+import { collectionsNameObj, dbConnect } from '@/lib/dbConnect';
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import React from 'react';
+import { FaArrowRightLong } from "react-icons/fa6";
 
-const ServicesSection = () => {
-    const [services, setServices] = useState([]);
-    console.log(services);
 
-    useEffect(() => {
-        const loadServices = async () => {
-            const res = await fetch('/services.json');
-            const data = await res.json();
-            setServices(data);
-        };
-
-        loadServices();
-    }, []);
+const ServicesSection = async () => {
+    const serviceCollection = dbConnect(collectionsNameObj().servicesCollection);
+    const data = await serviceCollection.find({}).toArray();
 
     return (
         <div className="max-w-6xl mx-auto px-4 py-10">
             <h2 className="text-3xl font-bold mb-6 text-center">Our Services</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {services.map((service, index) => (
+                {data.map((service, index) => (
                     <div key={index} className="card bg-base-100 shadow-md p-6 border">
-                        <Image src={service.img} width={314} height={208} alt={service.title} className='rounded-xl' />
+                        <Image
+                            src={service.img}
+                            width={314}
+                            height={208}
+                            alt={service.title}
+                            className="rounded-xl"
+                        />
                         <p className="text-gray-600 font-bold">{service.title}</p>
-                        <p className="text-red-600">Price : ${service.price}</p>
+                        <div className='flex justify-between mt-3'>
+                            <p className="text-red-600">Price : ${service.price}</p>
+                            <Link href={`/services/${service._id}`}>
+                                Details
+                            </Link>
+                        </div>
                     </div>
                 ))}
             </div>
